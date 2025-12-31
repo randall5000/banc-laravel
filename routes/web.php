3d303212bb4165dab2a714ed49f0d96ae6d72298
Randall L <randall.levere@gmail.com>
2025-12-31 15:31:48 -0500

    chore: Add debug route for /upload

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BenchController;
 
+// Debug Route: If this is hit, it means the Proxy rule in .htaccess failed or was ignored.
+Route::get('/upload', function () {
+    return 'DEBUG: Hit Laravel /upload route. Proxy to Next.js failed. Web Root is correct.';
+});
+
 Route::get('/', [BenchController::class, 'index'])->name('home');
 Route::redirect('/benches/create', '/upload')->name('benches.create');
 Route::post('/benches', [BenchController::class, 'store'])->name('benches.store');
